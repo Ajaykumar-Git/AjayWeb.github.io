82817630561fbdba75825a736fd712f134128702
Ajaykumar-Git <66913465+Ajaykumar-Git@users.noreply.github.com>
2020-06-26 18:51:35 +0530

    Ajaxcall added

---
+++ AjaxCall.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Script.Serialization;
+using System.Web.Script.Services;
+using System.Web.Services;
+
+public partial class AjaxCall : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    /*[WebMethod(enableSession: true)]
+    [ScriptMethod(ResponseFormat = ResponseFormat.Xml)]*/
+    [WebMethod]
+    public static string checkUserNameAvail(string iuser)
+    {
+        string is_unique = "false"; 
+        try
+        {
+            return is_unique;
+        }
+        catch
+        {
+            return is_unique;
+        }
+    } 
+
+}
---
+++ JS.js
@@ -0,0 +1,30 @@
+﻿
+
+
+$(document).ready(function () {
+
+    $('#ajaxBtn').click(function () {
+        checkUserNameAvail();
+    });
+
+    
+    function checkUserNameAvail() {
+        var userid = "AJay"//$("#reguser").val();  
+        $.ajax({
+            type: "POST",
+            url: "AjaxCall.aspx/checkUserNameAvail", //It calls our web method  
+            contentType: "application/json; charset=utf-8",
+            data: "{'iuser':'" + userid + "'}",
+            dataType: "json",
+            success: function (data) {      
+                alert(data.d);
+            },
+            error: function (d) {
+                debugger;
+
+                alert("error" + d);
+
+            }
+        });
+    }
+});
